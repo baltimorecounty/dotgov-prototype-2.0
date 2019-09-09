2c08e2889fe6b4e92aa7c2342b891b1c087dc43a
martypowell <jpowel25@gmail.com>
2019-09-09 11:32:19 -0400

    added routing and test css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "@baltimorecounty/dotgov-components/lib/styles/dotgov.min.css";
+import "./index.css";
 import Homepage from "./pages/HomePage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import PublicWorksLandingPage from "./pages/PublicWorksLandingPage";
 
 function App() {
-  return <Homepage />;
+  return (
+    <Router>
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/public-works" component={PublicWorksLandingPage} />
+    </Router>
+  );
 }
 
 export default App;
