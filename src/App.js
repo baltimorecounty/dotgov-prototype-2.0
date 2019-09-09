7cae3639781339041cd869258541452589ce66b7
martypowell <jpowel25@gmail.com>
2019-09-09 08:15:36 -0400

    moved script

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "@baltimorecounty/dotgov-components/lib/styles/dotgov.min.css";
-import Homepage from "./components/HomePage";
+import Homepage from "./pages/HomePage";
 
 function App() {
   return <Homepage />;
