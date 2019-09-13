c06dba417d8d527a1b0bb3f92bb1cfe4a3fe7660
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-09-13 08:25:04 -0400

    Added section compnent and image place holders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "@baltimorecounty/dotgov-components/lib/styles/dotgov.min.css";
-
+import SectionYourGovernment from "./Sections/SectionYourGovernment";
 function App() {
   return (
     <div className="App">
       <p>Welcome to the Baltimore County 2.0 Prototype</p>
+
+      <SectionYourGovernment></SectionYourGovernment>
     </div>
   );
 }
---
+++ src/Sections/SectionYourGovernment.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  IconHeading,
+  EmphasizedImage
+} from "@baltimorecounty/dotgov-components";
+const SectionYourGovernment = props => (
+  <div>
+    <div>
+      {/* <IconHeading text="Your Government" icon="fa fa-university"></IconHeading> */}
+    </div>
+    <div>
+      {/* <EmphasizedImage>
+        imageSrc="baltimorecountymd.gov/sebin/t/t/homepage-county-executive.jpg"
+        imageHeader="County Executive" 
+        imageSubheader="Johnny Olszewski"
+        imageLink = "www.w3schools.com"
+      </EmphasizedImage> */}
+      {/* <EmphasizedImage>
+        imageSrc="baltimorecountymd.gov/sebin/v/m/homepage-county-council.jpg"
+        imageHeader="County Executive" 
+        imageSubheader="Johnny Olszewski"
+        imageLink = "www.w3schools.com"
+      </EmphasizedImage> */}
+    </div>
+  </div>
+);
+
+export default SectionYourGovernment;
