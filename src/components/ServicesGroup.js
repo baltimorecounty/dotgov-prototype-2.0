429f4e829fc196c229b1e850ed65e5a9b3262db3
martypowell <jpowel25@gmail.com>
2019-09-13 10:00:42 -0400

    removed header

--- src/components/ServicesGroup.js
+++ src/components/ServicesGroup.js
@@ -2,21 +2,18 @@ import React from "react";
 import { IconLink } from "@baltimorecounty/dotgov-components";
 
 const ServicesGroup = props => {
-  const { services = [], title = "Most Popular Services" } = props;
+  const { services = [] } = props;
   return (
-    <div className="dg_popular-services">
-      <h2>{title}</h2>
-      <div className="dg_item-grid">
-        {services.map(service => (
-          <IconLink
-            key={service.id}
-            text={service.name}
-            href={service.href}
-            icon={service.icon}
-            description={service.description}
-          />
-        ))}
-      </div>
+    <div className="dg_item-grid">
+      {services.map(service => (
+        <IconLink
+          key={service.id}
+          text={service.name}
+          href={service.href}
+          icon={service.icon}
+          description={service.description}
+        />
+      ))}
     </div>
   );
 };
