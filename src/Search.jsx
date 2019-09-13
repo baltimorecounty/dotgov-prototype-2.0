946fb91362a8042ca743a6c1fd905cfb24d183fa
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-09-13 08:52:24 -0400

    Updating header to pull from components project

--- src/Search.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Search = props => <p>
-                            <span className="beta-text">Beta</span>
-                        </p>;
-
-export default Search
