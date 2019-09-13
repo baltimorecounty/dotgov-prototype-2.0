8c8c5fdbf91e091e6b2dea6252c381c7ad5f2453
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-13 13:03:20 -0400

    initial checkin

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
+import Transparency from './Transparency';
 import "@baltimorecounty/dotgov-components/lib/styles/dotgov.min.css";
-import "./index.css";
-import Homepage from "./pages/HomePage";
-import { HashRouter as Router, Route } from "react-router-dom";
-import PublicWorksLandingPage from "./pages/PublicWorksLandingPage";
+
 
 function App() {
-  return (
-    <Router>
-      <Route exact path="/" component={Homepage} />
-      <Route exact path="/public-works" component={PublicWorksLandingPage} />
-    </Router>
-  );
+  return (<Transparency/>);
 }
 
 export default App;
