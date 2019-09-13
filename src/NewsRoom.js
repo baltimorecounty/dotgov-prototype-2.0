e5039348e246d13f2cc109df126c7eb5b5f2c58a
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-13 14:11:55 -0400

    added button

--- src/NewsRoom.js
+++ src/NewsRoom.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { DateNewsCard, IconHeading } from '@baltimorecounty/dotgov-components';
+import { DateNewsCard, IconHeading, Button } from '@baltimorecounty/dotgov-components';
 
 const NewsRoom = (props) => (
-	<div>
+	<div style={{ padding: '10px' }} className="dg_section dark">
 		<IconHeading text="LATEST HEADLINES" icon="fa fa-newspaper" />
 		<DateNewsCard
 			date="07/29/2019"
@@ -10,6 +10,28 @@ const NewsRoom = (props) => (
 			snippet="Today Baltimore County officials joined with community and..."
 			link="//www.baltimorecountymd.gov/News/BaltimoreCountyNow"
 		/>
+		<DateNewsCard
+			date="07/29/2019"
+			headline="Baltimore County Marks Progress in Rehabilitation of Winters lane Homes"
+			snippet="Today Baltimore County officials joined with community and..."
+			link="//www.baltimorecountymd.gov/News/BaltimoreCountyNow"
+		/>
+		<DateNewsCard
+			date="07/29/2019"
+			headline="Baltimore County Marks Progress in Rehabilitation of Winters lane Homes"
+			snippet="Today Baltimore County officials joined with community and..."
+			link="//www.baltimorecountymd.gov/News/BaltimoreCountyNow"
+		/>
+		<DateNewsCard
+			date="07/29/2019"
+			headline="Baltimore County Marks Progress in Rehabilitation of Winters lane Homes"
+			snippet="Today Baltimore County officials joined with community and..."
+			link="//www.baltimorecountymd.gov/News/BaltimoreCountyNow"
+		/>
+
+		<div className="row mt-5" style={{ display: 'flex', justifyContent: 'space-between', padding: '10px' }}>
+			<Button text="VIEW ALL NEWS" className="dg_button-secondary" />
+		</div>
 	</div>
 );
 
