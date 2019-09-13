84163f73a209b842df7aadf92a4ee0a5c927846f
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-09-13 13:02:46 -0400

    Added Your Government section

--- src/Sections/SectionYourGovernment.js
+++ src/Sections/SectionYourGovernment.js
@@ -1,26 +1,35 @@
 import React from "react";
 import {
   IconHeading,
-  EmphasizedImage
+  EmphasizedImage,
+  GridExample
 } from "@baltimorecounty/dotgov-components";
 const SectionYourGovernment = props => (
   <div>
     <div>
-      {/* <IconHeading text="Your Government" icon="fa fa-university"></IconHeading> */}
+      <IconHeading text="Your Government" icon="fa fa-university"></IconHeading>
     </div>
-    <div>
-      {/* <EmphasizedImage>
-        imageSrc="baltimorecountymd.gov/sebin/t/t/homepage-county-executive.jpg"
-        imageHeader="County Executive" 
-        imageSubheader="Johnny Olszewski"
-        imageLink = "www.w3schools.com"
-      </EmphasizedImage> */}
-      {/* <EmphasizedImage>
-        imageSrc="baltimorecountymd.gov/sebin/v/m/homepage-county-council.jpg"
-        imageHeader="County Executive" 
-        imageSubheader="Johnny Olszewski"
-        imageLink = "www.w3schools.com"
-      </EmphasizedImage> */}
+    <div class="dg_section">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <EmphasizedImage
+              imageSrc="//baltimorecountymd.gov/sebin/t/t/homepage-county-executive.jpg"
+              imageHeader="County Executive"
+              imageSubHeader="Johnny Olszewski"
+              imageLink="https://www.w3schools.com"
+            ></EmphasizedImage>
+          </div>
+          <div class="col">
+            <EmphasizedImage
+              imageSrc="//baltimorecountymd.gov/sebin/v/m/homepage-county-council.jpg"
+              imageHeader="Legislature"
+              imageSubHeader="County Council"
+              imageLink="www.w3schools.com"
+            ></EmphasizedImage>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
