a8dedca8329135e8eb4902da2547715666d67b4c
martypowell <jpowel25@gmail.com>
2019-09-13 13:49:40 -0400

    adjusted layout so it displayed properly

--- src/Sections/SectionYourGovernment.js
+++ src/Sections/SectionYourGovernment.js
@@ -1,33 +1,32 @@
 import React from "react";
 import {
   IconHeading,
-  EmphasizedImage,
-  GridExample
+  EmphasizedImage
 } from "@baltimorecounty/dotgov-components";
 const SectionYourGovernment = props => (
-  <div>
-    <div>
-      <IconHeading text="Your Government" icon="fa fa-university"></IconHeading>
-    </div>
-    <div class="dg_section">
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <EmphasizedImage
-              imageSrc="//baltimorecountymd.gov/sebin/t/t/homepage-county-executive.jpg"
-              imageHeader="County Executive"
-              imageSubHeader="Johnny Olszewski"
-              imageLink="https://www.w3schools.com"
-            ></EmphasizedImage>
-          </div>
-          <div class="col">
-            <EmphasizedImage
-              imageSrc="//baltimorecountymd.gov/sebin/v/m/homepage-county-council.jpg"
-              imageHeader="Legislature"
-              imageSubHeader="County Council"
-              imageLink="www.w3schools.com"
-            ></EmphasizedImage>
-          </div>
+  <div class="dg_section">
+    <div class="container">
+      <div className="row">
+        <div className="col">
+          <IconHeading text="Your Government" icon="fa fa-university" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <EmphasizedImage
+            imageSrc="//baltimorecountymd.gov/sebin/t/t/homepage-county-executive.jpg"
+            imageHeader="County Executive"
+            imageSubHeader="Johnny Olszewski"
+            imageLink="https://www.w3schools.com"
+          ></EmphasizedImage>
+        </div>
+        <div class="col">
+          <EmphasizedImage
+            imageSrc="//baltimorecountymd.gov/sebin/v/m/homepage-county-council.jpg"
+            imageHeader="Legislature"
+            imageSubHeader="County Council"
+            imageLink="www.w3schools.com"
+          ></EmphasizedImage>
         </div>
       </div>
     </div>
