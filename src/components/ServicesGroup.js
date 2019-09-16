07a303ad585e078fbe6653c3c62492491f0eda2a
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-16 14:00:12 -0400

    added col formatting

--- src/components/ServicesGroup.js
+++ src/components/ServicesGroup.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { IconLink } from "@baltimorecounty/dotgov-components";
+import React from 'react';
+import { IconLink } from '@baltimorecounty/dotgov-components';
 
-const ServicesGroup = props => {
-  const { services = [] } = props;
-  return (
-    <div className="dg_item-grid">
-      {services.map(service => (
-        <IconLink
-          key={service.id}
-          text={service.name}
-          href={service.href}
-          icon={service.icon}
-          description={service.description}
-        />
-      ))}
-    </div>
-  );
+const ServicesGroup = (props) => {
+	const { services = [] } = props;
+	return (
+		<div className="dg_item-grid">
+			{services.map((service) => (
+				<IconLink
+					key={service.id}
+					text={service.name}
+					href={service.href}
+					icon={service.icon}
+					description={service.description}
+				/>
+			))}
+		</div>
+	);
 };
 
 export default ServicesGroup;
