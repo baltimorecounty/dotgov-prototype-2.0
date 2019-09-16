018cce6656120a6ab4bfb99938f55477fc212423
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-16 14:34:10 -0400

    fixed col display

--- src/components/NewsGroup.js
+++ src/components/NewsGroup.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { DateNewsCard,Button } from '@baltimorecounty/dotgov-components';
+import { DateNewsCard, Button } from '@baltimorecounty/dotgov-components';
 const NewsGroup = (props) => {
 	const { Newsitems = [] } = props;
 	return (
 		<React.Fragment>
-            <div className="row">
+			<div className="row">
 				{Newsitems.map((Newsitem) => (
-                    <div className="col col-lg-3">
-					<DateNewsCard
-						key={Newsitem.id}
-						date={Newsitem.date}
-						headline={Newsitem.headline}
-						snippet={Newsitem.snippet}
-						link={Newsitem.link}
-					/>
-                    </div>
+					<div className="col col-xl-3 col-lg-6">
+						<DateNewsCard
+							key={Newsitem.id}
+							date={Newsitem.date}
+							headline={Newsitem.headline}
+							snippet={Newsitem.snippet}
+							link={Newsitem.link}
+						/>
+					</div>
 				))}
 			</div>
-			 <div  style={{ display: 'flex', justifyContent: 'center', padding: '40px' }}>
+			<div style={{ display: 'flex', justifyContent: 'center', padding: '40px' }}>
 				<Button text="View all news" className="dg_button-secondary" />
-			</div> 
+			</div>
 		</React.Fragment>
 	);
 };
