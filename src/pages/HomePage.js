0e077391a747856fc26c614cc962c569983cf0c7
martypowell <jpowel25@gmail.com>
2019-09-16 09:32:58 -0400

    added updated site header

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,24 @@
 import React from "react";
 import MostPopularServices from "../components/MostPopularServices";
+import { SiteHeader } from "@baltimorecounty/dotgov-components";
 
 const HomePage = () => (
-  <div className="dg_section dark">
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <MostPopularServices />
+  <React.Fragment>
+    <SiteHeader
+      heading="Baltimore County"
+      subHeading="Government"
+      status="Beta"
+    />
+    <div className="dg_section dark">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <MostPopularServices />
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </React.Fragment>
 );
 
 export default HomePage;
