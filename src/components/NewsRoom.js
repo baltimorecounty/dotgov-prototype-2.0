b42a6da6df0525c27091468fa66ab9a098599dcc
martypowell <jpowel25@gmail.com>
2019-09-17 10:42:59 -0400

    adjusted content for tests

--- src/components/NewsRoom.js
+++ src/components/NewsRoom.js
@@ -5,8 +5,7 @@ const news = [
   {
     id: 1,
     date: "07/29/2019",
-    headline:
-      "Baltimore County Marks Progress in Rehabilitation of Winters lane Homes.",
+    headline: "Baltimore County Marks Progress in Rehabilitation",
     snippet: "Today Baltimore County officials joined with community and...",
     link: "//www.baltimorecountymd.gov/News/BaltimoreCountyNow"
   },
@@ -23,7 +22,7 @@ const news = [
     date: "07/29/2019",
     headline:
       "Baltimore County Marks Progress in Rehabilitation of Winters lane Homes",
-    snippet: "Today Baltimore County officials joined with community and...",
+    snippet: "Today...",
     link: "//www.baltimorecountymd.gov/News/BaltimoreCountyNow"
   },
   {
