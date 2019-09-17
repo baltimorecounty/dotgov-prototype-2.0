a6d2fd453e900049281f4dddd855d7a4567ebe50
Marty Powell <jepowell@baltimorecountymd.gov>
2019-09-17 12:01:58 -0400

    Updates

--- precache-manifest.a72ba445e6d3c57e777d5db7234bfb46.js
+++ precache-manifest.a72ba445e6d3c57e777d5db7234bfb46.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3691fb832fbd92c999438c25557ad6dd",
+    "revision": "e4e109a3c937642c3adc1ef6ae3309b9",
     "url": "/dotgov-prototype-2.0/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dotgov-prototype-2.0/static/css/2.718dbd6f.chunk.css"
   },
   {
-    "revision": "a91bcf5f3f3f2a292c6c",
+    "revision": "08f9c858b9d0256949b7",
     "url": "/dotgov-prototype-2.0/static/css/main.633fd7ef.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dotgov-prototype-2.0/static/js/2.4b6a1828.chunk.js"
   },
   {
-    "revision": "a91bcf5f3f3f2a292c6c",
-    "url": "/dotgov-prototype-2.0/static/js/main.d3b67037.chunk.js"
+    "revision": "08f9c858b9d0256949b7",
+    "url": "/dotgov-prototype-2.0/static/js/main.1329ad3c.chunk.js"
   },
   {
     "revision": "4510b8896649743ad0a2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dotgov-prototype-2.0/precache-manifest.f34ad8ddb090e8bfee7a2fefd9388937.js"
+  "/dotgov-prototype-2.0/precache-manifest.a72ba445e6d3c57e777d5db7234bfb46.js"
 );
 
 self.addEventListener('message', (event) => {
