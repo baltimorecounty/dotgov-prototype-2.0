de6950374c4c679e7fad83a65cdbc2b8c636ddf9
martypowell <jpowel25@gmail.com>
2019-09-17 11:51:06 -0400

    reformat

--- src/components/MostPopularServices.js
+++ src/components/MostPopularServices.js
@@ -1,91 +1,91 @@
-import React from 'react';
-import { IconHeading } from '@baltimorecounty/dotgov-components';
-import ServicesGroup from './ServicesGroup';
+import React from "react";
+import { IconHeading } from "@baltimorecounty/dotgov-components";
+import ServicesGroup from "./ServicesGroup";
 
 const services = [
-	{
-		id: 1,
-		name: 'Property Taxes',
-		href: '/property-taxes',
-		icon: 'fas fa-home',
-		description: 'View more information about property taxes.'
-	},
-	{
-		id: 2,
-		name: 'Pet Adoption',
-		href: '/pet-adoption',
-		icon: 'fas fa-heart',
-		description: 'View more information about pet adoption.'
-	},
-	{
-		id: 3,
-		name: 'Police News',
-		href: '/police-news',
-		icon: 'fas fa-shield',
-		description: 'View more police news.'
-	},
-	{
-		id: 4,
-		name: 'County Jobs',
-		href: '/jobs',
-		icon: 'fas fa-building',
-		description: 'View more information about Baltimore County jobs.'
-	},
-	{
-		id: 5,
-		name: 'County Code',
-		href: '/county-code',
-		icon: 'fas fa-clipboard-check',
-		description: 'View more information about County Code.'
-	},
-	{
-		id: 6,
-		name: 'Trash Recycling',
-		href: '/trash',
-		icon: 'fas fa-dumpster',
-		description: 'View more information about trash and recycling.'
-	},
-	{
-		id: 7,
-		name: 'Pay Tickets',
-		href: '/pay-tickets',
-		icon: 'fas fa-car',
-		description: 'Pay your Baltimore county speeding and parking tickets.'
-	},
-	{
-		id: 8,
-		name: 'Property Taxes',
-		href: '/property-taxes',
-		icon: 'fas fa-home',
-		description: 'View more information about property taxes.'
-	},
-	{
-		id: 9,
-		name: 'County Parks',
-		href: '/county-parks',
-		icon: 'fas fa-tree',
-		description: 'View more information about county parks.'
-	},
-	{
-		id: 10,
-		name: 'View All Services',
-		href: '/services',
-		icon: 'fas fa-list-ul',
-		description: 'View all Baltimore County services.'
-	}
+  {
+    id: 1,
+    name: "Property Taxes",
+    href: "/property-taxes",
+    icon: "fas fa-home",
+    description: "View more information about property taxes."
+  },
+  {
+    id: 2,
+    name: "Pet Adoption",
+    href: "/pet-adoption",
+    icon: "fas fa-heart",
+    description: "View more information about pet adoption."
+  },
+  {
+    id: 3,
+    name: "Police News",
+    href: "/police-news",
+    icon: "fas fa-shield",
+    description: "View more police news."
+  },
+  {
+    id: 4,
+    name: "County Jobs",
+    href: "/jobs",
+    icon: "fas fa-building",
+    description: "View more information about Baltimore County jobs."
+  },
+  {
+    id: 5,
+    name: "County Code",
+    href: "/county-code",
+    icon: "fas fa-clipboard-check",
+    description: "View more information about County Code."
+  },
+  {
+    id: 6,
+    name: "Trash Recycling",
+    href: "/trash",
+    icon: "fas fa-dumpster",
+    description: "View more information about trash and recycling."
+  },
+  {
+    id: 7,
+    name: "Pay Tickets",
+    href: "/pay-tickets",
+    icon: "fas fa-car",
+    description: "Pay your Baltimore county speeding and parking tickets."
+  },
+  {
+    id: 8,
+    name: "Property Taxes",
+    href: "/property-taxes",
+    icon: "fas fa-home",
+    description: "View more information about property taxes."
+  },
+  {
+    id: 9,
+    name: "County Parks",
+    href: "/county-parks",
+    icon: "fas fa-tree",
+    description: "View more information about county parks."
+  },
+  {
+    id: 10,
+    name: "View All Services",
+    href: "/services",
+    icon: "fas fa-list-ul",
+    description: "View all Baltimore County services."
+  }
 ];
 
 const MostPopularServices = () => (
-	<React.Fragment>
-		<div className="dg_section dark">
-			<IconHeading
-				text="Most Popular Services"
-				image="//staging.baltimorecountymd.gov/sebin/h/i/seal-color-74.png"
-			/>
+  <React.Fragment>
+    <div className="dg_section dark">
+      <IconHeading
+        text="Most Popular Services"
+        image="//staging.baltimorecountymd.gov/sebin/h/i/seal-color-74.png"
+      />
 
-			<ServicesGroup services={services} />
-		</div>
-	</React.Fragment>
+      <ServicesGroup services={services} />
+    </div>
+  </React.Fragment>
 );
 
 export default MostPopularServices;
--- src/components/Transparency.js
+++ src/components/Transparency.js
@@ -1,44 +1,46 @@
-import React from 'react';
-import TransparencyGroup from './TransparencyGroup';
+import React from "react";
+import TransparencyGroup from "./TransparencyGroup";
 const transparencyItems = [
-	{
-		id: 1,
-		icon: 'fa fa-bullhorn',
-		size: 'large',
-		type: 'circle',
-		description: 'Visit our users dashboard to learn more.',
-		text: 'PUBLIC NOTICES',
-		href: 'https://www.w3schools.com'
-	},
-	{
-		id: 2,
-		icon: 'fa fa-calendar-alt',
-		size: 'large',
-		type: 'circle',
-		description: 'Visit our users dashboard to learn more.',
-		text: 'MEETING AND EVENTS',
-		href: 'https://www.w3schools.com'
-	},
-	{
-		id: 3,
-		icon: 'fa fa-clipboard',
-		type: 'circle',
-		size: 'large',
-		description: 'Visit our users dashboard to learn more.',
-		text: 'BOARDS AND COMMISSIONS',
-		href: 'https://www.w3schools.com'
-	},
-	{
-		id: 4,
-		icon: 'fa fa-chart-line',
-		type: 'circle',
-		size: 'large',
-		description: 'Visit our users dashboard to learn more.',
-		text: 'OPEN DATA',
-		href: 'https://www.w3schools.com'
-	}
+  {
+    id: 1,
+    icon: "fa fa-bullhorn",
+    size: "large",
+    type: "circle",
+    description: "Visit our users dashboard to learn more.",
+    text: "PUBLIC NOTICES",
+    href: "https://www.w3schools.com"
+  },
+  {
+    id: 2,
+    icon: "fa fa-calendar-alt",
+    size: "large",
+    type: "circle",
+    description: "Visit our users dashboard to learn more.",
+    text: "MEETING AND EVENTS",
+    href: "https://www.w3schools.com"
+  },
+  {
+    id: 3,
+    icon: "fa fa-clipboard",
+    type: "circle",
+    size: "large",
+    description: "Visit our users dashboard to learn more.",
+    text: "BOARDS AND COMMISSIONS",
+    href: "https://www.w3schools.com"
+  },
+  {
+    id: 4,
+    icon: "fa fa-chart-line",
+    type: "circle",
+    size: "large",
+    description: "Visit our users dashboard to learn more.",
+    text: "OPEN DATA",
+    href: "https://www.w3schools.com"
+  }
 ];
 
-const Transparency = () => <TransparencyGroup transparencyItems={transparencyItems} />;
+const Transparency = () => (
+  <TransparencyGroup transparencyItems={transparencyItems} />
+);
 
 export default Transparency;
