f8b5464cc006c5b345e90b80eab44503d6d46ac4
martypowell <jpowel25@gmail.com>
2019-09-17 11:50:04 -0400

    adjusted layout

--- src/components/TransparencyGroup.js
+++ src/components/TransparencyGroup.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { IconLink } from '@baltimorecounty/dotgov-components';
+import React from "react";
+import { IconLink } from "@baltimorecounty/dotgov-components";
 
-const TransparencyGroup = (props) => {
-	const { transparencyItems = [] } = props;
-	return (
-		<React.Fragment>
-			{transparencyItems.map((transparencyItem) => (
-				<div className="col-xl-3 col-lg-6">
-					<IconLink
-						id={transparencyItem.id}
-						icon={transparencyItem.icon}
-						type={transparencyItem.type}
-						size={transparencyItem.size}
-						description={transparencyItem.description}
-						text={transparencyItem.text}
-						href={transparencyItem.href}
-					/>
-				</div>
-			))}
-		</React.Fragment>
-	);
+const TransparencyGroup = props => {
+  const { transparencyItems = [] } = props;
+  return (
+    <React.Fragment>
+      {transparencyItems.map(transparencyItem => (
+        <div className="col-lg-3 col-sm-6 d-lg-flex">
+          <IconLink
+            id={transparencyItem.id}
+            icon={transparencyItem.icon}
+            type={transparencyItem.type}
+            size={transparencyItem.size}
+            description={transparencyItem.description}
+            text={transparencyItem.text}
+            href={transparencyItem.href}
+          />
+        </div>
+      ))}
+    </React.Fragment>
+  );
 };
 
 export default TransparencyGroup;
