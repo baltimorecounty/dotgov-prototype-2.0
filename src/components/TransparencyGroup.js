a9a5207d983f5d5c066077c7c4095261342266c4
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-17 11:16:04 -0400

    removing grid

--- src/components/TransparencyGroup.js
+++ src/components/TransparencyGroup.js
@@ -5,21 +5,19 @@ const TransparencyGroup = (props) => {
 	const { transparencyItems = [] } = props;
 	return (
 		<React.Fragment>
-			<div className="row">
-				{transparencyItems.map((transparencyItem) => (
-					<div className="col col-xl-3 col-lg-6">
-						<IconLink
-							id={transparencyItem.id}
-							icon={transparencyItem.icon}
-							type={transparencyItem.type}
-							size={transparencyItem.size}
-							description={transparencyItem.description}
-							text={transparencyItem.text}
-							href={transparencyItem.href}
-						/>
-					</div>
-				))}
-			</div>
+			{transparencyItems.map((transparencyItem) => (
+				<div className="col-xl-3 col-lg-6">
+					<IconLink
+						id={transparencyItem.id}
+						icon={transparencyItem.icon}
+						type={transparencyItem.type}
+						size={transparencyItem.size}
+						description={transparencyItem.description}
+						text={transparencyItem.text}
+						href={transparencyItem.href}
+					/>
+				</div>
+			))}
 		</React.Fragment>
 	);
 };
