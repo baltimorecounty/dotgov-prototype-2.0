1c3269dee908d550033dc04e030bbe9cc7c94faa
martypowell <jpowel25@gmail.com>
2019-09-17 10:39:42 -0400

    fixed formatting

--- src/components/sections/NewsRoomSection.js
+++ src/components/sections/NewsRoomSection.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import NewsRoom from '../NewsRoom';
-import { Button, Section, IconHeading } from '@baltimorecounty/dotgov-components';
-const ctaButton = <Button text="View all news" className="dg_button-secondary" />;
-const NewsRoomSection = (props) => (
-	<Section className="dark" ctaButton={ctaButton}>
-		<div className="container">
-			<div className="row">
-				<div className="col">
-					<IconHeading text="Latest Headlines" icon="far fa-newspaper" />
-				</div>
-			</div>
-			<div className="row">
-				<NewsRoom />
-			</div>
-		</div>
-	</Section>
+import React from "react";
+import NewsRoom from "../NewsRoom";
+import {
+  Button,
+  Section,
+  IconHeading
+} from "@baltimorecounty/dotgov-components";
+
+const ctaButton = (
+  <Button text="View all news" className="dg_button-secondary" />
+);
+
+const NewsRoomSection = props => (
+  <Section className="dark" ctaButton={ctaButton}>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <IconHeading text="Latest Headlines" icon="far fa-newspaper" />
+        </div>
+      </div>
+      <div className="row">
+        <NewsRoom />
+      </div>
+    </div>
+  </Section>
 );
 
 export default NewsRoomSection;
