a18707a0063d3ab1869d2868d50027b24064d14a
martypowell <jpowel25@gmail.com>
2019-09-24 14:05:42 -0400

    added pageheader to prototype

--- src/templates/DefaultInternalTemplate.js
+++ src/templates/DefaultInternalTemplate.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { PageHeader } from "@baltimorecounty/dotgov-components";
 
 const DefaultInternalTemplate = props => {
   const { MainContent, SideBar, mainContentTitle, sideBarTitle } = props;
   return (
     <div className="dg_internal-template">
+      <PageHeader
+        title="Public Works"
+        preTitle="Department of"
+        deck="The Department of Public Works oversees and maintains the County’s public infrastructure, including highways, utilities, and trash and recycling."
+        className="blue"
+        backGroundImage="//baltimorecountymd.gov/sebin/j/x/public-works-header.jpg"
+      />
       <div className="container">
         <div className="row">
           <div className="col-md-8 col-sm-12">
