da68f58a692f74a26c37f9c439b3dcd62a08f99e
Marty Powell <jepowell@baltimorecountymd.gov>
2019-10-16 09:43:01 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dotgov-prototype-2.0/precache-manifest.5b00d2238e7127120d2d418a01618656.js"
+  "/dotgov-prototype-2.0/precache-manifest.1646f0395712d85f298588441f181974.js"
 );
 
 self.addEventListener('message', (event) => {
