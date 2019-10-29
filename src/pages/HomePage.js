1f29ea6ae2dbc9858f0b35d89d906ed2d285845d
martypowell <jpowel25@gmail.com>
2019-10-29 07:56:12 -0400

    setup template for hopempage with site nav

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Alert } from "@baltimorecounty/dotgov-components";
+import SiteNav from "../components/SiteNav";
 import SiteHeader from "../components/SiteHeader";
 import SiteFooter from "../components/SiteFooter";
 import MostPopularServicesSection from "../components/sections/MostPopularServicesSection";
@@ -9,19 +10,22 @@ import YourGovernmentSection from "../components/sections/YourGovernmentSection"
 
 const HomePage = () => (
   <React.Fragment>
-    <SiteHeader />
-    <Alert>
-      <p>
-        The water cooler outside of the Budget and Finance Office has been moved
-        to the Historic Courthouse Mezzanine. For more information please visit
-        our <a href="/closings">Closings Page</a>
-      </p>
-    </Alert>
-    <MostPopularServicesSection />
-    <YourGovernmentSection />
-    <NewsRoomSection />
-    <TransparencySection />
-    <SiteFooter />
+    <SiteNav />
+    <div id="bc_page">
+      <SiteHeader />
+      <Alert>
+        <p>
+          The water cooler outside of the Budget and Finance Office has been
+          moved to the Historic Courthouse Mezzanine. For more information
+          please visit our <a href="/closings">Closings Page</a>
+        </p>
+      </Alert>
+      <MostPopularServicesSection />
+      <YourGovernmentSection />
+      <NewsRoomSection />
+      <TransparencySection />
+      <SiteFooter />
+    </div>
   </React.Fragment>
 );
 
