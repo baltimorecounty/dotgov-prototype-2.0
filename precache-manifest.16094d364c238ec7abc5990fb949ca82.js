d1fcef96f0c7f37a60759bee8520d053c3ad599e
Marty Powell <jepowell@baltimorecountymd.gov>
2019-11-04 13:55:52 -0500

    Updates

---
+++ precache-manifest.16094d364c238ec7abc5990fb949ca82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62692b95b8b4a678d4409191f6cfbd0f",
+    "url": "/dotgov-prototype-2.0/index.html"
+  },
+  {
+    "revision": "1cc8ca4b26f78b717db5",
+    "url": "/dotgov-prototype-2.0/static/css/main.d55cfd46.chunk.css"
+  },
+  {
+    "revision": "04a289a18e4d60f51275",
+    "url": "/dotgov-prototype-2.0/static/js/2.88139a00.chunk.js"
+  },
+  {
+    "revision": "1cc8ca4b26f78b717db5",
+    "url": "/dotgov-prototype-2.0/static/js/main.93ae7704.chunk.js"
+  },
+  {
+    "revision": "4510b8896649743ad0a2",
+    "url": "/dotgov-prototype-2.0/static/js/runtime~main.7057d25c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dotgov-prototype-2.0/precache-manifest.0bdc01d099ebac74cc371cc6bd5c6224.js"
+  "/dotgov-prototype-2.0/precache-manifest.16094d364c238ec7abc5990fb949ca82.js"
 );
 
 self.addEventListener('message', (event) => {
