338243ddb871b8c7a6258044692fa2a03efb4e74
martypowell <jpowel25@gmail.com>
2019-11-04 14:57:39 -0500

    adjusted relative links to absolute so they would work

--- src/components/SiteNav.js
+++ src/components/SiteNav.js
@@ -2,12 +2,38 @@ import React from "react";
 import { SiteNavigation } from "@baltimorecounty/dotgov-components";
 
 const links = [
-  { href: "/public-works", text: "Public Works", icon: "far fa-trash" },
-  { href: "/adoptable-pets", text: "Adoptable Pets", icon: "far fa-paw" },
-  { href: "/images", text: "Images", icon: "far fa-images" },
-  { href: "/accordions", text: "Accordions", icon: "far fa-angle-down" },
-  { href: "/alerts", text: "Alerts", icon: "far fa-exclamation-triangle" },
-  { href: "/cards", text: "Cards", icon: "far fa-id-card" }
+  {
+    href:
+      "https://baltimorecounty.github.io/dotgov-prototype-2.0/#/public-works",
+    text: "Public Works",
+    icon: "far fa-trash"
+  },
+  {
+    href:
+      "https://baltimorecounty.github.io/dotgov-prototype-2.0/#/adoptable-pets",
+    text: "Adoptable Pets",
+    icon: "far fa-paw"
+  },
+  {
+    href: "https://baltimorecounty.github.io/dotgov-prototype-2.0/#/images",
+    text: "Images",
+    icon: "far fa-images"
+  },
+  {
+    href: "https://baltimorecounty.github.io/dotgov-prototype-2.0/#/accordions",
+    text: "Accordions",
+    icon: "far fa-angle-down"
+  },
+  {
+    href: "https://baltimorecounty.github.io/dotgov-prototype-2.0/#/alerts",
+    text: "Alerts",
+    icon: "far fa-exclamation-triangle"
+  },
+  {
+    href: "https://baltimorecounty.github.io/dotgov-prototype-2.0/#/cards",
+    text: "Cards",
+    icon: "far fa-id-card"
+  }
 ];
 
 const SiteNav = props => <SiteNavigation links={links} />;
